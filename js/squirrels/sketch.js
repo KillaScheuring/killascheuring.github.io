f92254d5ad1e80d85ad6f595dd256566f5c6a568
Killashandra Scheuring <killashandra@sylvia.com>
2020-03-05 13:13:28 -0600

    added winning and loosing and restart

--- js/squirrels/sketch.js
+++ js/squirrels/sketch.js
@@ -1,74 +1,255 @@
-let squirrels = Array(10);
-const offset = 50;
+let squirrels = [];
+let game_state = "START";
+const INITIAL_SQUIRRELS = 10;
+const MAX_SQUIRRELS = 20;
+let squirrelNames = [];
+
+let car = {};
+
+const POSSIBLE_NAMES = [
+    "Joey",
+    "Mickey",
+    "Thomas",
+    "Daniel",
+    "Dan",
+    "Tom",
+    "Benjamin",
+    "Ben",
+    "Adam",
+    "Samuel",
+    "Sammy",
+    "John",
+    "Luke",
+    "James",
+    "Jamie",
+    "Nathan",
+    "Joshua",
+    "Josh",
+    "Jacob",
+    "Aaron",
+    "David",
+    "Dave",
+    "Matthew",
+    "Matt",
+    "Ethan",
+    "Robert",
+    "Bob",
+    "Robbie",
+    "Oliver",
+    "Ollie",
+    "Jack",
+    "William",
+    "Richard",
+    "Ricky",
+    "Rick",
+    "Dick",
+    "Rich",
+    "Harry",
+    "Christopher",
+    "Chris",
+    "Harrison",
+    "Jake",
+    "Edward",
+    "Charles",
+    "Charlie",
+    "Dewey",
+    "Liam",
+    "Henry",
+    "Heinz",
+    "Louis",
+    "Marcus",
+    "Angel",
+    "Rhodes",
+    "Jesse",
+    "Joel",
+    "Alvan",
+    "Zechariah",
+    "Malachi",
+    "Jasper",
+    "Mark",
+    "Amber",
+    "Martha",
+    "Phoebe",
+    "Elizabeth",
+    "Liza",
+    "Hannah",
+    "Anna",
+    "Abigail",
+    "Chloe",
+    "Mary",
+    "Rebecca",
+    "Becca",
+    "Sarah",
+    "Sadie",
+    "Emily",
+    "Emma",
+    "Millie",
+    "Holly",
+    "Jessica",
+    "Jessie",
+    "Jess",
+    "Poppy",
+    "Freya",
+    "Isabella",
+    "Bella",
+    "Nicole",
+    "Niki",
+    "Amelia",
+    "Florence",
+    "Ellie",
+    "Molly",
+    "Sophie",
+    "Alice",
+    "Allie",
+    "Eleanor",
+    "Leora",
+    "Mia",
+    "Jasmine",
+    "Ella",
+    "Amy",
+    "Megan",
+    "Meg",
+    "Zoe",
+    "Adria",
+    "Sha",
+    "Mariah",
+    "Brook",
+    "Tabitha",
+    "Naomi",
+    "Olive",
+    "Claudia",
+    "Jo-Anna",
+    "Deborah",
+    "Ira",
+    "Livia",
+    "Magdalene",
+    "Alena",
+    "Micah ",
+    "Mia",
+    "Liz"];
 
 function setup() {
     // Set up canvas
-    createCanvas(500, 500);
-
-    // Generate initial squirrels
-    for(let i = 0; i < squirrels.length; i++){
-        // Get this squirrel's spawn location
-        let spawn = pickRandomSpawn();
-        // create the squirrel
-        squirrels[i] = new Squirrel(spawn.x, spawn.y);
-        // Set this squirrel in motion
-        squirrels[i].moveToTarget(width/2, (height));
-    }
+    createCanvas(1000, 750);
+    car = {
+        lives: 3,
+        h: 50,
+        w: 60,
+        x: width / 2 - 50,
+        y: (height - 50),
+        win: false
+    };
 }
 
 function draw() {
-    // Color background
-    background(76, 199, 70);
-
-    // Occasionally generate more squirrels until max reached
-    if(random() < 0.03 && squirrels.length < 40){
-        // Get the generated squirrel's spawn location
-        let spawn = pickRandomSpawn();
-        // create the squirrel
-        let newSquirrel = new Squirrel(spawn.x, spawn.y);
-        // push to the squirrel array
-        squirrels.push(newSquirrel);
-        // Set this squirrel in motion
-        newSquirrel.moveToTarget(width/2, (height));
+    if (game_state === "START") {
+        background(228, 230, 140);
+        fill(0);
+        text("Click to start", width / 2 - width / 10, height / 2);
+        if (mouseIsPressed) {
+            game_state = "RUNNING";
+        }
     }
+    else if (game_state === "RUNNING") {
+        // Color background
+        background(76, 199, 70);
 
-    // Loop through the squirrels, update them, and show their location
-    for(let squirrel of squirrels){
-        squirrel.update();
-        squirrel.show();
+        // Occasionally generate more squirrels until max reached
+        if (random() < 0.03 && squirrels.length < MAX_SQUIRRELS) {
+            // select the generated squirrel's spawn location
+            let spawn = pickRandomSpawn();
+            // create the squirrel
+            let newSquirrel = new Squirrel(spawn.x, spawn.y);
+            // push to the squirrel array
+            squirrels.push(newSquirrel);
+            // Set this squirrel in motion
+            newSquirrel.moveToTarget(car.x, car.y);
+        }
 
-        // If squirrel finished death animation, remove from array
-        if(squirrel.color[3] <= 0){
-            squirrels.splice(squirrels.indexOf(squirrel), 1);
+        // Loop through the squirrels, update them, and show their location
+        for (let squirrel of squirrels) {
+            squirrel.update();
+            squirrel.show();
+            if (dist(squirrel.x, squirrel.y, car.x, car.y) < squirrel.r*1.1) {
+                car.lives--;
+                squirrels.splice(squirrels.indexOf(squirrel), 1);
+            }
+            // If squirrel finished death animation, remove from array
+            if (squirrel.color[3] <= 0) {
+                squirrelNames.push(squirrel.name);
+                squirrels.splice(squirrels.indexOf(squirrel), 1);
+            }
+        }
+        if(car.lives <= 0){
+            game_state = "GAME_OVER";
+            squirrels = [];
+        }
+        if(squirrelNames.length > 10){
+            game_state = "GAME_OVER";
+            squirrels = [];
+            car.win = true;
+        }
+    }
+    else if (game_state === "GAME_OVER") {
+        background(171, 171, 171);
+        fill(0);
+        text("Game Over Click to try again in", width / 2 - width / 10, height / 2);
+        text("Game Over Click to try again in", width / 2 - width / 10, height / 2  + 20);
+        text("Squirrels Killed", width / 2 - width / 10, height / 2 + 30);
+
+        if (mouseIsPressed) {
+            squirrelNames = [];
+            car.lives = 3;
+            car.win = false;
+            game_state = "RUNNING";
         }
     }
 
     // Draw target and "car"
     fill(0);
-    rect(width/2-50, (height-50), 100, 50);
+    rect(car.x, car.y, car.w, car.h);
     fill(0, 255, 0);
-    ellipse(width/2, (height-50), 5, 5);
+    ellipse(car.x, car.y, 5, 5);
+}
+
+// Generates a name and has a chance to add a flourish
+function pickRandomName() {
+    let name = random(POSSIBLE_NAMES);
+    if (random() > 0.95) {
+        let newName = "That ";
+        newName += random() > 0.5 ? "bastard " : "b**** ";
+        name = newName + name;
+    }
+
+    console.log(name);
+
+    return name;
+
 }
 
 // Generates a spawn point on part of a circle around the center
-function pickRandomSpawn(){
+function pickRandomSpawn() {
     // get a random angle on the circle
-    let randomAngle = random((Math.PI/2) + (Math.PI/8), ((3*Math.PI)/2)-(Math.PI/8));
+    let randomAngle = random((Math.PI / 2) + (Math.PI / 8), ((3 * Math.PI) / 2) - (Math.PI / 8));
 
     // get the x and y position from that angle
-    let xPos = (Math.sin(randomAngle)*(width+100)/2+width/2);
-    let yPos = (Math.cos(randomAngle)*(height+100)/2)+height/2;
+    let xPos = (Math.sin(randomAngle) * (width + 100) / 2 + width / 2);
+    let yPos = (Math.cos(randomAngle) * (height + 100) / 2) + height / 2;
 
     // return a vector with those x and y positions
     return createVector(xPos, yPos);
 }
 
 // check if squirrel is clicked
-function mouseClicked(){
+function mouseClicked() {
     // Loop through squirrels
-    for(let i = 0; i < squirrels.length; i++){
+    for (let i = 0; i < squirrels.length; i++) {
         // ask this squirrel if it got hit
+
         squirrels[i].hit(mouseX, mouseY);
+    }
+    if (game_state === "RUNNING") {
+
     }
 }
 
--- js/squirrels/squirrel.js
+++ js/squirrels/squirrel.js
@@ -4,17 +4,42 @@ class Squirrel {
         // set position
         this.x = x;
         this.y = y;
+        this.name = pickRandomName();
 
         // set velocity to (0, 0)
         this.v = createVector();
 
-        // set color, default https://www.color-hex.com/color/a0522d
-        this.color = [160, 82, 45, 255];
+
         // set size of the squirrel
         this.r = 20;
 
+        this.level = random();
+        if (this.level > 0.99) {
+            this.level = 5;
+        }
+        else if (this.level > 0.95) {
+            this.level = 4;
+        }
+        else if (this.level > 0.80) {
+            this.level = 3;
+        }
+        else if (this.level > 0.60) {
+            this.level = 2;
+        }
+        else {
+            this.level = 1;
+        }
+            console.log("this.level", this.level);
+
+        // higher level squirrels are faster
+        this.speed = 0.50 * this.level;
+
+        // set color, default https://www.color-hex.com/color/a0522d
+        // 160, 82, 45
+        this.color = [160 - 40 * this.level, 82 - 20 * this.level, 45 - 50 * this.level, 255];
+
         // set number of lives (will be determined by level later)
-        this.lives = 2;
+        this.lives = this.level;
         // current stun time
         this.stunned = 0;
     }
@@ -26,6 +51,9 @@ class Squirrel {
         fill(this.color[0], this.color[1], this.color[2], this.color[3]);
         // currently a circle
         ellipse(this.x, this.y, this.r * 2, this.r * 2);
+
+        fill(255);
+        text(this.level, this.x, this.y);
     }
 
     // set velocity
@@ -42,17 +70,17 @@ class Squirrel {
     // update squirrel's info
     update() {
         // run death animation if lives is less than or equal to zero
-        if(this.lives <= 0){
+        if (this.lives <= 0) {
             // Current death animation is turning redder and fading the alpha
             this.color[3] -= 10;
         } // run stunned animation
-        else if(this.stunned > 0){
+        else if (this.stunned > 0) {
             // Current stunned animation is getting redder then fading to default color
-            this.color[0] = 160+(this.stunned*5);
+            this.color[0] = 160 - 40 * this.level + (this.stunned * 5);
             this.stunned -= 1;
         } else {
             //  set red to default in case steps are off in the stunned animation
-            this.color[0] = 160;
+            this.color[0] = 160 - 40 * this.level;
             // if no other condition move by velocity
             this.x += this.v.x;
             this.y += this.v.y;
@@ -64,17 +92,16 @@ class Squirrel {
     moveToTarget(xPos, yPos) {
         let vPos = createVector(xPos, yPos);
         let vectorDiff = createVector(vPos.x - this.x, vPos.y - this.y);
-        let amplitude = Math.pow(vectorDiff.x * vectorDiff.x + vectorDiff.y* vectorDiff.y, .5)*2;
-        console.log(amplitude);
-        let newVector = createVector(vectorDiff.x/amplitude, vectorDiff.y/amplitude);
+        let amplitude = Math.pow(vectorDiff.x * vectorDiff.x + vectorDiff.y * vectorDiff.y, .5) / this.speed;
+        let newVector = createVector(vectorDiff.x / amplitude, vectorDiff.y / amplitude);
         this.setVelocity(newVector.x, newVector.y);
     }
 
 
     // sets info if killed
-    killed(){
+    killed() {
         // turned redder
-        this.color[0] = 160+(this.stunned*5);
+        this.color[0] = 160 + (this.stunned * 5);
         // stops moving
         this.v = createVector();
     }
@@ -88,13 +115,15 @@ class Squirrel {
         // if mouse within 10% of the squirrel, it's a hit
         if (d < this.r * 1.1) {
             // set stunned time 30 frames
-            this.stunned = 30;
+            this.stunned = 200 / this.level;
 
             // reduces lives by one
             this.lives -= 1;
 
             // checks if killed
-            if(this.lives <= 0){this.killed()}
+            if (this.lives <= 0) {
+                this.killed()
+            }
 
             // returns hit true
             return true;
